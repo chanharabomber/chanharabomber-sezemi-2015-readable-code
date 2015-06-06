89bce11554479dbbe2c5cb57dd9c63d99762185b
chanhara <yusuke.hara.github@gmail.com>
2015-06-06 11:48:14 +0900

    spec:3 ファイルの読込み機能を追加

--- recipe.java
+++ recipe.java
@@ -1,8 +1,35 @@
+/**
+*   chanharabomber
+*   レシピデータをテキストファイルから読み込んで
+*   表示するプログラム
+**/
+
+//fileIO処理のためのクラスのimport
+import java.io.*;
+
+1
 class recipe {
 
   public static void main(String[] args) {
-    
-    System.out.println("オムライス");
+
+    try {
+
+      File recipeFile = new File("" + args[0] );
+      BufferedReader br = new BufferedReader(new FileReader(recipeFile));
+      String readLine = br.readLine();
+      while(readLine != null) {
+          System.out.println(readLine);
+          
+          readLine = br.readLine();
+      }
+      
+      br.close();
+
+    } catch(FileNotFoundException e) {
+      System.out.println(e);
+    } catch(IOException e) {
+      System.out.println(e);
+    }
 
   }
 
